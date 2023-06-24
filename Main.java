ea5d8aafb410e8e0945a72921f319023b86fade3
W. Machado <104407345+winissius@users.noreply.github.com>
2023-06-24 00:39:51 -0300

    Implementado metodo bubble recursivo

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int[] vetor = {3,6,8,1,4,9,0};
         // int[] vetor = {10, 15, 6, 25, 3, 5};
-        BubbleRecursive.bubbleSort(vetor);
-        //Selection.selectionSort(vetor);
+        // BubbleRecursive.bubbleSort(vetor);
+        Selection.selectionSort(vetor);
     }
 }
